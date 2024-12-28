86dc4abdeb4b7dc5f18e242cd08fafaad1395b67
Masahiko Funaki <mfunaki@mayoct.net>
2024-12-28 15:33:41 +0900

    Add sample Python script demonstrating Selenium usage

---
+++ Chap01/ch01_sample01.py
@@ -0,0 +1,11 @@
+# Python program to demonstrate
+# selenium
+
+# import webdriver
+from selenium import webdriver
+
+# create webdriver object
+driver = webdriver.Chrome()
+
+# Get google.com
+driver.get("https://www.google.com")
