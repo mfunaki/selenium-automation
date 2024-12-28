01496970876ab63fefb6dc632b804e4493aa790d
Masahiko Funaki <mfunaki@mayoct.net>
2024-12-28 16:53:11 +0900

    Add sample Selenium script to search for "Arrays" on GeeksforGeeks

---
+++ Chap03/ch03_sample01.py
@@ -0,0 +1,15 @@
+# import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# create webdriver object
+driver = webdriver.Chrome()
+
+# get geeksforgeeks.org
+driver.get("https://www.geeksforgeeks.org")
+
+# get element
+element = driver.find_element(By.ID, "gsc-i-id1")
+
+# send keys
+element.send_keys("Arrays")
